8656f3b7474cebc63b63d0c46dee3df1a4e13580
EricHeredia <EricDavidHeredia@gmail.com>
2019-01-01 15:47:31 -0600

    added styling

--- src/App.js
+++ src/App.js
@@ -1,244 +1,308 @@
 import React, { Component } from 'react';
 import './App.css';
+import update from 'immutability-helper'
 import math from 'mathjs'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      display: '0',
-      numDisplay: '0',
-      isOping: false,
-      newTotal: ''
+      input: [],
+      inputDisplay: ['0'],
+      display: ['0'],
+      currentNumber: []
     }
-    this.numberClick = this.numberClick.bind(this)
-    this.operationClick = this.operationClick.bind(this)
-    this.equalsClick = this.equalsClick.bind(this)
-    this.decimalClick = this.decimalClick.bind(this)
-    this.clearScreen = this.clearScreen.bind(this)
   }
-  
-  numberClick(e) {
-    if (this.state.isOping && this.state.newTotal) {
-      this.setState({
-        display: this.state.display + e.target.innerHTML,
-        numDisplay: e.target.innerHTML,
-        isOping: false,
-        newTotal: ''
+
+  handleClick = (e) => {
+    const value = e.target.getAttribute('value')
+    const lastInput = this.state.input[this.state.input.length - 1]
+    
+    switch (value) {
+      case 'clear':
+        this.setState({
+          input: [],
+          inputDisplay: ['0'],
+          display: ['0'],
+          currentNumber: []
+        })
+        break
+      case 'equals':
+        this.calculateInput()
+        break
+      case '.':
+      if(/[=]/.test(String(this.state.inputDisplay))) {
+        this.setState({
+          input: ['0', '.'],
+          inputDisplay: ['0', '.'],
+          display: ['0', '.'],
+          currentNumber: ['0', '.']
+        })
+      } else if (!/[.]/.test(this.state.currentNumber)) {
+            this.defaultInput(e)
+          }
+        break
+      case '0':
+        if (this.state.currentNumber.length > 0) {
+          this.defaultInput(e)
+        }
+        break
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        if (/[/*\-+]/.test(lastInput)) {
+          const repInput = update(this.state.input, {
+            $splice: [[this.state.input.length - 1, 1, value]]
+          })
+          this.setState({
+            input: repInput,
+            inputDisplay: repInput,
+            display: repInput,
+            currentNumber: []
+          })
+        } else if (this.state.currentNumber.length) {
+          this.defaultInput(e)
+        }
+        break
+      default:
+        this.defaultInput(e)
+        break
+    }
+  }
+
+  defaultInput(e) {
+    const value = e.target.getAttribute('value')
+    if (/[/*\-+]/.test(value)) {
+      const newInput = update(this.state.input, {
+        $push: [value]
       })
-    } else if (this.state.display === '0' || this.state.newTotal) {
       this.setState({
-        display: e.target.innerHTML,
-        numDisplay: e.target.innerHTML,
-        isOping: false,
-        newTotal: ''
+        input: newInput,
+        inputDisplay: newInput,
+        display: this.state.display,
+        currentNumber: []
       })
-    } else if (this.state.numDisplay.indexOf('.') !== -1) {
-      this.setState({
-        display: this.state.display + e.target.innerHTML,
-        numDisplay: this.state.numDisplay + e.target.innerHTML,
-        isOping: false,
-        newTotal: '',
-      })
+    } else if (/[0-9]/.test(value) && /[=]/.test(this.state.inputDisplay)) {
+      if (value === '0') {
+        this.setState({
+          input: [],
+          inputDisplay: [value],
+          display: [value],
+          currentNumber: []
+        })
+      } else {
+        this.setState({
+        input: [value],
+        inputDisplay: [value],
+        display: [value],
+        currentNumber: [value]
+        })
+      }
+    } else if (value === '.' && this.state.currentNumber.length === 0) {
+      const newInput = update(this.state.input, {
+        $push: ['0' + value]
+        })
+        this.setState({
+          input: newInput,
+          inputDisplay: newInput,
+          display: newInput,
+          currentNumber: newInput
+        })
     } else {
+      const newInput = update(this.state.input, {
+      $push: [value]
+      })
+      const newCurrentNumber = update(this.state.currentNumber, {
+      $push: [value]
+      })
       this.setState({
-        display: this.state.display + e.target.innerHTML,
-        numDisplay: e.target.innerHTML,
-        isOping: false,
-        newTotal: ''
+        input: newInput,
+        inputDisplay: newInput,
+        display: newCurrentNumber,
+        currentNumber: newCurrentNumber
       })
     }
   }
 
-  operationClick(e) {
-    if (this.state.display === '0') {
-      // Do nothing.
-    } else if (this.state.newTotal && this.state.display !== '0') {
-      this.setState({
-        display: this.state.newTotal + e.target.innerHTML,
-        isOping: true
-      })
-    } else if (this.state.display !== '0' && this.state.isOping === false) {
-      this.setState({
-        display: this.state.display + e.target.innerHTML,
-        isOping: true
-      })
-    } else if (this.state.display[this.state.display.length - 1] !== e.target.innerHTML) {
-      this.setState({
-        display: this.state.display.slice(0, -1) + e.target.innerHTML
-      })
+  calculateInput = () => {
+    let total = this.state.input.join('')
+    if (total) {
+      try {
+        total = math.eval(total)
+        total = math.format(total, {precision: 7})
+        total = String(total)
+        this.setState({
+          input: [total],
+          inputDisplay: this.state.input.join('') + '=' + total,
+          display: [total],
+          currentNumber: [total]
+        })
+      } catch (e) {
+        this.setState({
+          input: [],
+          inputDisplay: this.state.input.join('') + '= Error',
+          display: 'Error'
+        })
+      }
     }
-  }
-
-  equalsClick(e) {
-    if (this.state.display === '0') {
-      this.clearScreen()
-    } else if (this.state.display.indexOf('=') === -1 && !this.state.isOping) {
-      const total = math.eval(this.state.display)
-      this.setState({
-        display: this.state.display + e.target.innerHTML + total,
-        numDisplay: total,
-        newTotal: total
-      })
-    }
-  }
-
-  decimalClick(e) {
-    if (this.state.numDisplay.indexOf('.') === -1 && this.state.display === '0') {
-      this.setState({
-        display: e.target.innerHTML,
-        numDisplay: e.target.innerHTML
-      })
-    } else {
-      this.setState({
-        display: this.state.display + e.target.innerHTML,
-        numDisplay: this.state.numDisplay + e.target.innerHTML
-      })
-    }
-  }
-
-  clearScreen() {
-    this.setState({
-      display: '0',
-      numDisplay: '0'
-    })
   }
 
   render() {
     return (
       <div className="App">
-        <div>
-          <h1
-            id="display"
-          >
-          {this.state.display}
-          </h1>
+        <div id="full-inputDisplay">
+          <div>
+            <p
+              id="inputDisplay"
+            >
+            {this.state.inputDisplay}
+            </p>
+          </div>
+  
+          <div>
+            <h1
+              id="display"
+              maxlength="10"
+            >
+            {this.state.display}
+            </h1>
+          </div>
         </div>
 
-        <div>
-          <h1
-            id="numDisplay"
-          >
-          {this.state.numDisplay}
-          </h1>
-        </div>
-
-        <div>
+        <div id='calcButtons'>
           <button
             id="clear"
-            onClick={this.clearScreen}
+            value="clear"
+            onClick={this.handleClick}
           >
           AC
           </button>
 
           <button
             id="divide"
-            onClick={this.operationClick}
+            value="/"
+            onClick={this.handleClick}
           >
           /
           </button>
 
           <button
             id="multiply"
-            onClick={this.operationClick}
+            value="*"
+            onClick={this.handleClick}
           >
           *
           </button>
 
           <button
             id="seven"
-            onClick={this.numberClick}
+            value="7"
+            onClick={this.handleClick}
           >
           7
           </button>
 
           <button
             id="eight"
-            onClick={this.numberClick}
+            value="8"
+            onClick={this.handleClick}
           >
           8
           </button>
 
           <button
             id="nine"
-            onClick={this.numberClick}
+            value="9"
+            onClick={this.handleClick}
           >
           9
           </button>
 
           <button
             id="subtract"
-            onClick={this.operationClick}
+            value="-"
+            onClick={this.handleClick}
           >
           -
           </button>
 
           <button
             id="four"
-            onClick={this.numberClick}
+            value="4"
+            onClick={this.handleClick}
           >
           4
           </button>
 
           <button
             id="five"
-            onClick={this.numberClick}
+            value="5"
+            onClick={this.handleClick}
           >
           5
           </button>
 
           <button
             id="six"
-            onClick={this.numberClick}
+            value="6"
+            onClick={this.handleClick}
           >
           6
           </button>
 
           <button
             id="add"
-            onClick={this.operationClick}
+            value="+"
+            onClick={this.handleClick}
           >
           +
           </button>
 
           <button
             id="one"
-            onClick={this.numberClick}
+            value="1"
+            onClick={this.handleClick}
           >
           1
           </button>
 
           <button
             id="two"
-            onClick={this.numberClick}
+            value="2"
+            onClick={this.handleClick}
           >
           2
           </button>
 
           <button
             id="three"
-            onClick={this.numberClick}
+            value="3"
+            onClick={this.handleClick}
           >
           3
           </button>
 
           <button
             id="equals"
-            onClick={this.equalsClick}
+            value="equals"
+            onClick={this.handleClick}
           >
           =
           </button>
 
           <button
             id="zero"
-            onClick={this.numberClick}
+            value="0"
+            onClick={this.handleClick}
           >
           0
           </button>
 
           <button
             id="decimal"
-            onClick={this.decimalClick}
+            value="."
+            onClick={this.handleClick}
           >
           .
           </button>
